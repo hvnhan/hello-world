b275acf7c0d90cde138db2ced729cfb5439702a8
hvnhan <hvnhan@users.noreply.github.com>
2023-04-23 18:13:16 +0700

    Update fb.js

--- fb.js
+++ fb.js
@@ -1,4 +1,13 @@
 /// fbads.js
-(function(){
-  document.body.style.background = "yellow";
+(function() {
+    const start = () => {
+       document.body.style.background = "yellow";
+    };
+    if ( document.readyState !== 'complete' && /\bcomplete\b/.test(behavior) ) {
+        self.addEventListener('load', start, { once: true });
+    } else if ( document.readyState === 'loading' ) {
+        self.addEventListener('DOMContentLoaded', start, { once: true });
+    } else {
+        start();
+    }
 })();
